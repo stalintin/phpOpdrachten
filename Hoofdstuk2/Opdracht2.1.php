388d5964515ac92127b647f1451d2ae78c680e3a
Valentin Böck <valentin.bock@edu-kw1c.nl>
2020-02-10 12:10:26 +0100

    Opdracht2.1

---
+++ Hoofdstuk2/Opdracht2.1.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * User: Naam
+ * Date: Datum
+ * Time: Time
+ * File: index.php
+ */?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php print "Pagina 1"; ?></title>
+    <link href="../stylesheet.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2</li>
+        <ul>
+            <li>
+                <a href="Hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
+            </li>
+            <li>
+                <a href="Hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
+            </li>
+        </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -19,13 +19,13 @@
     <aside>
         <h2>Menu</h2>
         <ul>
-            <li>Hoofdstuk 2</li>
+            <li>Hoofdstuk 2
                 <ul>
                     <li>
-                        <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                        <a href="Hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
                     </li>
                     <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                        <a href="Hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
                     </li>
                 </ul>
             </li>
