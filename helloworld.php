460b68363dbbf990522f32cf443821e451291d9f
Valentin Böck <valentin.bock@edu-kw1c.nl>
2020-02-10 11:16:02 +0100

    hello world

--- helloworld.php
+++ helloworld.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8"/>
-        <title><?php print "Pagina 1"; ?></title>
-    </head>
-    <body><h1>
-<?php
-        echo  "Hallo Wereld";
-?>
-    </h1>
+<head>
+    <meta charset="utf-8"/>
+    <title><?php print "Pagina 1"; ?></title>
+</head>
+<body><h1>
+    <?php
+    echo  "Hallo Wereld";
+    ?>
+</h1>
 
-    </body>
+</body>
 </html>
