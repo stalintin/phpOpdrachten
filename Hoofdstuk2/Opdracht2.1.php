71898127314032f07296599c07e26335eb83afd0
Valentin Böck <valentin.bock@edu-kw1c.nl>
2020-02-14 11:49:42 +0100

    Opdracht 2.1

--- Hoofdstuk2/Opdracht2.1.php
+++ Hoofdstuk2/Opdracht2.1.php
@@ -16,21 +16,19 @@
 <header>
     <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
 </header>
-<aside>
     <h2>Menu</h2>
     <ul>
         <li>Hoofdstuk 2</li>
         <ul>
             <li>
-                <a href="Hoofdstuk2/Opdracht2.1.php">Opdracht 2.1</a>
+                <a href="Opdracht2.1.php">Opdracht 2.1</a>
             </li>
             <li>
-                <a href="Hoofdstuk2/Opdracht2.2.php">Opdracht 2.2</a>
+                <a href="Opdracht2.2.php">Opdracht 2.2</a>
             </li>
         </ul>
         </li>
     </ul>
-</aside>
 <main id="wrapper">
     <h2>Uitwerkingen</h2>
 </main>
