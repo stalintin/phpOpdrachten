ee308cc517bdbfb0f2a98587288ba5111c0bf8ee
Valentin Böck <valentin.bock@edu-kw1c.nl>
2020-02-17 11:12:22 +0100

    Opdracht 2.1 2.0

--- Hoofdstuk2/Opdracht2.1.php
+++ Hoofdstuk2/Opdracht2.1.php
@@ -16,71 +16,13 @@
 <header>
     <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
 </header>
-    <h2>Menu</h2>
+    <p><?php echo "Hello World!" ?></p>
     <ul>
-        <li>Hoofdstuk 2</li>
-        <ul>
-            <li>
-                <a href="Opdracht2.1.php">Opdracht 2.1</a>
-            </li>
-            <li>
-                <a href="Opdracht2.2.php">Opdracht 2.2</a>
-            </li>
-        </ul>
-        </li>
-        <li>Hoofdstuk 3
-            <ul>
-                <li>
-                    <a href="Hoofdstuk3/">Opdracht 3.1</a>
-                </li>
-                <li>
-                    <a href="Hoofdstuk3/">Opdracht 3.2</a>
-                </li>
-                <li>
-                    <a href="Hoofdstuk3/">Opdracht 3.3</a>
-                </li>
-
-            </ul>
-        </li>
-        <li>Hoofdstuk 4
-            <ul>
-                <li>
-                    <a href="Hoofdstuk4/">Opdracht 4.1</a>
-                </li>
-                <li>
-                    <a href="Hoofdstuk4/">Opdracht 4.2</a>
-                </li>
-                <li>
-                    <a href="Hoofdstuk4/">Opdracht 4.3</a>
-                </li>
-                <li>
-                    <a href="Hoofdstuk4/">Opdracht 4.4</a>
-                </li>
-            </ul>
-        </li>
-        <li>Hoofdstuk 5
-            <ul>
-                <li>
-                    <a href="Hoofdstuk5/">Opdracht 5.1</a>
-                </li>
-                <li>
-                    <a href="Hoofdstuk5/">Opdracht 5.2</a>
-                </li>
-                <li>
-                    <a href="Hoofdstuk5/">Opdracht 5.3</a>
-                </li>
-                <li>
-                    <a href="Hoofdstuk5/">Opdracht 5.4</a>
-                </li>
-            </ul>
+        <li>
+            <a href="../index.php">Home</a>
         </li>
     </ul>
-<main id="wrapper">
-    <h2>Uitwerkingen</h2>
-</main>
 </body>
 </html>
-<?php
-echo  "Hallo Wereld";
-?>
+
 
