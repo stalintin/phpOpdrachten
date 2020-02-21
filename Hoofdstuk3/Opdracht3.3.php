9a24dde86b455d9433934d1d3f1751dc400d1a10
Valentin Böck <valentin.bock@edu-kw1c.nl>
2020-02-21 13:15:14 +0100

    Opdracht3.3

---
+++ Hoofdstuk3/Opdracht3.3.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<head>
+    <title></title>
+    <style>
+        <?php include '../../css/style.css'; ?>
+    </style>
+</head>
+
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="../../Hoofdstuk%202/2.1/opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="../../Hoofdstuk%202/2.2/opdracht2.2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 3
+            <ul>
+                <li>
+                    <a href="../3.1/opdracht3.1.php">Opdracht 3.1</a>
+                </li>
+                <li>
+                    <a href="../3.2/opdracht3.2.php">Opdracht 3.2</a>
+                </li>
+                <li>
+                    <a href="opdracht3.3.php">Opdracht 3.3</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+
+
+
+</main>
+</body>
