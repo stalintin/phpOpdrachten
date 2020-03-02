128cdcd211f2cab84731346cf5f702da4ac220b0
Valentin Böck <valentin.bock@edu-kw1c.nl>
2020-03-02 12:01:17 +0100

    Opdracht 3.1

--- Hoofdstuk3/Opdracht3.1.php
+++ Hoofdstuk3/Opdracht3.1.php
@@ -25,28 +25,28 @@
 </header>
 <div class="box">
     <?php
-    $elf = "<u>Elfstedentocht</u>";
-    $alve = "<u>Alvestêdetocht</u>";
+    $evenement = "<u>Elfstedentocht</u>";
+    $eventfries = "<u>Alvestêdetocht</u>";
     $km = "<u>200</u>";
     $schaats = "<u>schaatstocht</u>";
     $natuur = "<u>natuurijs</u>";
-    $kvdfes = "<u>Koninklijke Vereniging De Friesche Elf Steden</u>";
-    $leeuw = "<u>Leeuwarden</u>";
-    $fries = "<u>Friesland</u>";
-    $num = "<u>15</u>";
+    $verenigingsnaam = "<u>Koninklijke Vereniging De Friesche Elf Steden</u>";
+    $stad = "<u>Leeuwarden</u>";
+    $provincie = "<u>Friesland</u>";
+    $number = "<u>15</u>";
     $jaar = "<u>1909</u>";
-    $verhaal = "De $elf (Fries: $alve) is een $km
+    $verhaal = "De $evenement (Fries: $eventfries) is een $km
                 kilometer lange $schaats over $natuur die wordt
-                georganiseerd door de $kvdfes. $leeuw, de hoofdstad van $fries, is start- en
-                aankomstplaats. De $elf is inmiddels $num maal
+                georganiseerd door de $verenigingsnaam. $stad, de hoofdstad van $provincie, is start- en
+                aankomstplaats. De $evenement is inmiddels $number maal
                 verreden en werd voor het eerst in $jaar gereden en wordt
                 maximaal 1 keer per winter gehouden.";
     echo ("<h1>Taak 1</h1>");
     echo ("<p>$verhaal</p>");
 
-    $verhaal2 = "De " . $elf .  " (Fries: " . $alve . ") is een " . $km . " kilometer lange " . $schaats . " over " . $natuur . " die wordt
-                georganiseerd door de " . $kvdfes .". " . $leeuw . ",  de hoofdstad van " . $fries . ",  is start- en
-                aankomstplaats. De " . $elf . " is inmiddels " . $num . " maal
+    $verhaal2 = "De " . $evenement .  " (Fries: " . $eventfries . ") is een " . $km . " kilometer lange " . $schaats . " over " . $natuur . " die wordt
+                georganiseerd door de " . $verenigingsnaam .". " . $stad . ",  de hoofdstad van " . $provincie . ",  is start- en
+                aankomstplaats. De " . $evenement . " is inmiddels " . $number . " maal
                 verreden en werd voor het eerst in " . $jaar . " gereden en wordt
                 maximaal 1 keer per winter gehouden.";
     echo ("<h1>Taak 2</h1>");
