04f720ef3b7a395ed48a7d2327b9919df3355409
Valentin Böck <valentin.bock@edu-kw1c.nl>
2020-03-20 12:09:15 +0100

    header tweak

--- Includes/Header.php
+++ Includes/Header.php
@@ -6,7 +6,7 @@
     <meta name="description" content=" ">
     <meta name="keywords" content=" ">
     <meta charset="utf-8">
-    <link href="../CSS/stylesheet.css.css" type="text/css" rel="stylesheet">
+    <link href="../CSS/stylesheet.css" type="text/css" rel="stylesheet">
 </head>
 <body>
 <h2>Uitwerking van <a href="../index.php"> PHP-opdrachten</a></h2>
